d5f66c907a9f53336e994565f6b3b8521e8dc11b
TegarArdi <tegarardirohman@gmail.com>
2024-03-14 15:37:19 +0700

    install MERN

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-3xl text-red-500'>
+      hehehe 
+    </h1>
+  )
+}
+
+export default App
