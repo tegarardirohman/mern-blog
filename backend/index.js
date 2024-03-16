68b6cca124ac05d71c73ad6537c90414230c4212
TegarArdi <tegarardirohman@gmail.com>
2024-03-16 13:53:04 +0700

    mongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("MongoDB is connected")
+})
+.catch(err => {
+    console.log("Cannot connect to the DB" + err)
+});
 
 const app = express();
 
