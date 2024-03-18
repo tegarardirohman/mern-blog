50d290e93470d9c8c7c415682ece0652f39b2cf6
TegarArdi <tegarardirohman@gmail.com>
2024-03-18 12:39:56 +0700

    signup: insert data

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +16,12 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ()=> {
     console.log('Server berjalan di port 3000');
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
